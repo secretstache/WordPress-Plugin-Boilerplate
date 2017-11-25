32b31d29de196d2343dd0caadedfd473c36316c6
Rama Dimasatria <ramadimasatria@gmail.com>
2017-11-25 13:54:50 +0700

    Setup react on public site

--- plugin-name/webpack.config.js
+++ plugin-name/webpack.config.js
@@ -1,15 +1,31 @@
 const path = require('path')
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 
-const publicEntry = 'js/plugin-name-public'
+const publicEntry = 'js/index'
 const adminEntry = 'js/plugin-name-admin'
 
 module.exports = {
   entry: {
-    [`public/build/${publicEntry}`]: `./public/${publicEntry}.js`,
-    [`admin/build/${adminEntry}`]: `./admin/${adminEntry}.js`
+    [`public/build/js/bundle`]: `./public/${publicEntry}.js`,
+    [`admin/build/js/bundle`]: `./admin/${adminEntry}.js`
   },
   output: {
-    filename: '[name].bundle.js',
+    filename: '[name].js',
     path: path.resolve(__dirname)
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env', 'react']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [new UglifyJSPlugin()]
 }
